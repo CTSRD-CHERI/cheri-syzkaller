e1ec8c57b6cb61446cf3eb62e591088845cf47e8
zalan <mazalan01@gmail.com>
2022-07-20 16:14:52 +0100

    changed sysroot and ccompiler for freebsd arm64

--- sys/targets/targets.go
+++ sys/targets/targets.go
@@ -339,10 +339,10 @@ var List = map[string]map[string]*Target{
 			PtrSize:        8,
 			PageSize:       4 << 10,
 			LittleEndian:   true,
-			CCompiler:      "clang",
+			CCompiler:      "/home/zalan/cheri/output/morello-sdk/bin/clang",
 			CFlags:		[]string{
 				"-target", "aarch64-unknown-freebsd14.0",
-				"--sysroot", "/home/at718/cheri/build/freebsd-aarch64-build/usr/home/at718/cheri/freebsd/arm64.aarch64/tmp",
+				"--sysroot", "/usr/home/zalan/cheri/output/rootfs-morello-purecap",
 			},
 			NeedSyscallDefine: func(nr uint64) bool {
 				// freebsd_12_shm_open, shm_open2, shm_rename, __realpathat, close_range, copy_file_range
